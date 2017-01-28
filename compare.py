9579658e2c45fd162f582ca3731963b8a2be2102
Colton <colton.r.lathrop@gmail.com>
2017-01-28 12:32:55 -0600

    working release

--- compare.py
+++ compare.py
@@ -1,36 +1,85 @@
+import os
 #compare tool to find delta between two directories
 #two diretories are required - working and test
 #the working directory will be taking from a working store with similar settings
 #a similar store can be found via bigfix exploration and filters
 
-testdir = 'test'
-workingdir = 'working'
-testlist = []
-workinglist = []
+testList = 0
+workingList = 0
+resultsTxt = 0
+cwd = os.getcwd()
+cwdTest = os.getcwd() + '/test'
+cwdWorking = os.getcwd() + '/working'
+iniTest = 0
+iniWorking = 0
+yy = 0
+temp = 0
+outputList = 0
 
-
-#open matching first file reading line by line creating list
-#open matching second file line by line creating list
-#write file to results that you are comparing
-#results compare list writing lines from test on left side working on right side separated by ::::
-#once loop is done closes results.txt saves it
-
-def results():
+def resultTxt():
+    global resultsTxt
+    os.remove('results.txt')
+    resultsTxt = open('results.txt', 'a')
+    return
     #createresults() if results.txt doesnt exist
     #clears results.txt
     #openresults()
 
-def dirlist():
+
+def dirList():
+    global testList
+    global workingList
+    testList = [a for a in os.listdir(cwdTest) if a.endswith('.ini')]
+    workingList = [a for a in os.listdir(cwdWorking) if a.endswith('.ini')]
     #open test directory and create list of files
     #open working directory and create list of files
+    #print workingList
+    #print testList
+    
 
-def getlists(a,b):
+def gLists(testInput,workingInput):
+    global resultsTxt
+    global iniTest
+    global iniWorking
+    os.chdir(cwdTest)
+    iniTest = open(testInput).readlines()
+    os.chdir(cwdWorking)
+    iniWorking = open(workingInput).readlines()
+    resultsTxt.write(testInput + '\n')
     #resets prior lists
     #create lists of both files line by line
-    #two inputs a being test b being working with same name
+    #two inputs working files and test files
+    #writes the test ini to the doc
+    return
 
-def compare(c,d):
-    #starts by writing the common file name on the doc
-    #c being test d being working
+def compare(list1, list2):
+    global outputList
+    outputList = []
+    list3 = list1 + list2
+    for i in range(0, len(list3)):
+        if ((list3[i] not in list1) or (list3[i] not in list2)) and (list3[i] not in outputList):
+             outputList[len(outputList):] = [list3[i]]
+    for h in outputList:
+        print 'success1'
+        resultsTxt.write("%s\n" % outputList)
+    #yy = set(compWorking)
+    #temp = [x for x in compTest if x not in yy]
     #compare lists line by lines
     #writing any differences to results.txt separated by ::::
+    return
+
+resultTxt()
+dirList()
+totalIni = len(testList)
+startIni = 0
+curIni = 0
+for x in range(startIni, totalIni):
+    gLists(testList[curIni], workingList[curIni])
+    curIni += 1
+    compare(iniTest, iniWorking)
+    
+    
+    
+    
+    
+    
