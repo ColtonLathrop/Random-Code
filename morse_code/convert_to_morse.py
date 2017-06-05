a518b444d04be617d548e4a5047551057f3bf4c5
Colton <colton.r.lathrop@gmail.com>
2017-06-05 17:54:03 -0500

    Add files via upload

--- morse_code/convert_to_morse.py
+++ morse_code/convert_to_morse.py
@@ -1,5 +1,6 @@
 from ConfigParser import SafeConfigParser
 import string
+import os
 
 target = 'target.txt'
 
@@ -18,17 +19,55 @@ def parse_ini():
 def convert_target():
     with open(target, "r") as targetfile:
         targetstring = targetfile.read()
+        templist = list()
     for i in xrange(0, len(targetstring)):
-        print targetstring[i]
-        if any(character in targetstring)
+        #print targetstring[i]
+        templist.append(targetstring[i])
+    outputfile = open('output.txt', "a")
+    for i in templist:
+        try:
+            a = combined_alphabet.index(i)
+        except ValueError:
+            outputfile.write(" ")
+        outputfile.write(combined_morse[a])
 
-    pass
+    '''for x in templist:
+        tempvar0 = 0
+        tempvar1 = 0
+        for z in capital_alphabet:
+            if templist[x] == capital_alphabet[tempvar1]:
+                tempvar0 += 1
+                outputfile.write(morselist[tempvar0])
+            else:
+                tempvar1 += 1
+                pass
+        tempvar0 = 0
+        tempvar1 = 0
+        for y in lower_alphabet:
+            if templist[x] == lower_alphabet[y]:
+                outputfile.write(morselist[tempvar0])
+            else:
+                tempvar1 += 1
+                pass
+        if templist[x] == ' ':
+            outputfile.write(' ')
+        else:
+            pass'''
+
 
 
 morselist = parse_ini()
 #print morselist
 capital_alphabet = list(string.ascii_uppercase)
 lower_alphabet = list(string.ascii_lowercase)
+combined_alphabet = capital_alphabet + lower_alphabet
+combined_morse = morselist + morselist
+
+
 #print capital_alphabet
 #print lower_alphabet
+print combined_alphabet
+print combined_morse
+a = combined_alphabet.index('a')
+print a
 convert_target()
